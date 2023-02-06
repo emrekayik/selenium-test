6b0a43e598638378f1b78b174d4a105c0d2da02f
emrekayik <emrekayik1905@gmail.com>
2023-02-07 02:33:39 +0300

    ilk test bitti

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-const {Builder} = require('selenium-webdriver');
+const { Builder } = require("selenium-webdriver");
 require("chromedriver");
 
 (async function helloSelenium() {
-  let driver = await new Builder().forBrowser('chrome').build();
+  let driver = await new Builder().forBrowser("chrome").build();
+  driver.manage().window().maximize();
 
-  await driver.get('https://selenium.dev');
+  await driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
+  await driver.sleep(500);
+  await driver.actions().scroll(10, 10, 0, 400).perform();
+  
 
+  // fill in form
+  await driver.findElement({ name: "firstname" }).sendKeys("John");
+  await driver.findElement({ name: "lastname" }).sendKeys("Doe");
+
+  // select radio button
+  await driver.findElement({ id: "sex-0" }).click();
+  await driver.findElement({ id: "exp-3" }).click();
+  
+  // fill in date
+  await driver.findElement({ id: "datepicker" }).sendKeys("07/02/2023");
+  
+  // select elements
+  await driver.findElement({ id: "profession-0" }).click();
+  await driver.findElement({ id: "profession-1" }).click();
+  
+  // scroll down
+  await driver.actions().scroll(10, 10, 0, 200).perform();
+  
+  // select elements
+  await driver.findElement({ id: "tool-0" }).click();
+  await driver.findElement({ id: "tool-1" }).click();
+  await driver.findElement({ id: "tool-2" }).click();
+  
+  // scroll down
+  await driver.actions().scroll(10, 10, 0, 200).perform();
+  
+  // select elements
+  await select(driver.findElement({ id: "continents" })).selectByVisibleText("Europe");
+  await select(driver.findElement({ id: "selenium_commands" })).selectByVisibleText("Browser Commands");
+  
+  // click on submit button
+  await driver.findElement({ id: "submit" }).click();
+  
+  // wait for 5 seconds
+  await driver.sleep(5000)
+  
   await driver.quit();
 })();
-
-/*
-
-(async function example() {
-  driver = await new Builder().forBrowser("chrome").build();
-  await driver.get("https://www.google.com");
-  await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
-  await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
-  await driver.quit();
-})();
-
-*/
